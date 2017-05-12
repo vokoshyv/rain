6aa545e0005c4bf133cc9c049113a187d0f17953
vokoshyv <ronsuij@gmail.com>
2017-05-11 23:23:28 -0700

    (feat) implement difference between stateless and stateful components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+// const App = () => {
+//   return <h1>Hello</h1>
+// }
 
-export default App
+/*
+  This one here can have state
+ */
+// class App extends React.Component {
+//   render(){
+//     return <h1>Hello World</h1>
+//   }
+// }
+
+/*
+  This one here cannot have state
+  Don't think I'll use this one as often
+ */
+const App = () => <h1>Hello Stateless</h1>
+
+export default App;
